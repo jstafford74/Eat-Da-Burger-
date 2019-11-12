f04dee016f9ce8ef0972f63ccd7590359074d163
Jeremiah Stafford <jstafford@slmdevice.com>
2019-11-12 17:05:27 -0500

    setup orm and models

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+-- Drops the todolist if it exists currently --
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Creates the "todolist" database --
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+CREATE TABLE burgers
+(
+  ID int NOT NULL
+  AUTO_INCREMENT,
+burger_name  VARCHAR
+  (255),
+devoured BOOLEAN,
+PRIMARY KEY
+  (ID));
+
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Queso');
+INSERT INTO burgers (burger_name) VALUES ('Cheddar');
+INSERT INTO burgers (burger_name) VALUES ('California');
+INSERT INTO burgers (burger_name) VALUES ('Bacon');
+INSERT INTO burgers (burger_name) VALUES ('Avocado');
+INSERT INTO burgers (burger_name) VALUES ('Turkey');
