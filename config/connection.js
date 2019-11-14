dbe2dcac8dfd9a21f4d0b267a0d024a8529f3973
Jeremiah Stafford <jstafford@slmdevice.com>
2019-11-14 18:30:24 -0500

    css fix

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 console.log(process.env.NODE_ENV === 'development', process.env.NODE_ENV)
-const connection = mysql.createConnection(process.env.JAWSDB_URL);
+const connection = mysql.createConnection(require('./db-config'));
 
 // Make connection.
 connection.connect(function (err) {
